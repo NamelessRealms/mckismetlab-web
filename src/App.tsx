942cfd41548ba090bbf6bbc61a2197f619954518
Quasi <quasipersonal58@gmail.com>
2022-05-07 16:30:31 +0800

    Finish: team page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Front from "./renderer/pages/front/Front";
 import Launcher from "./renderer/pages/launcher/Launcher";
 import ModServer from "./renderer/pages/modServer/ModServer";
 import Sponsor from "./renderer/pages/sponsor/Sponsor";
+import Team from "./renderer/pages/team/Team";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/modServer" element={<ModServer />} />
         <Route path="/sponsor" element={<Sponsor />} />
         <Route path="/launcher" element={<Launcher />} />
+        <Route path="/team" element={<Team />} />
       </Routes>
     </BrowserRouter>
   );
