530e41a579a0a87c91e08d03f07b87c85a70eff0
Quasi <quasipersonal58@gmail.com>
2022-05-24 13:36:46 +0800

    Add: static props revalidate 60s

--- pages/launcher/index.tsx
+++ pages/launcher/index.tsx
@@ -205,6 +205,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
     return {
         props: {
             launcher: launcherData
-        }
+        },
+        revalidate: 60
     }
 }
